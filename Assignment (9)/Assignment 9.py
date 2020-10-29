e45738ec5d921f582561afb78c2292f3adec4a8e
Nathanael126 <71549317+Nathanael126@users.noreply.github.com>
2020-10-29 13:48:39 +0700

    Add files via upload

--- Assignment (9)/Assignment 9.py
+++ Assignment (9)/Assignment 9.py
@@ -1,1 +1,27 @@
 Sentence = "Mr. Miyagi bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it. Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true... Well, with a probability of .9 it isn't."
+Paragraph = []
+counter1 = 0
+Sentences = []
+Titles = ["Mr.","Mrs.","Dr."]
+
+for a in Sentence.split():
+    counter1 += 1
+    try:
+        if a[-1] == "!" or a[-1] == "?":
+            Sentences.append(a)
+            x = " ".join(map(str, Sentences))
+            Paragraph.append(x)
+            Sentences = []
+        elif a[-1] == "." and Sentence.split()[counter1][0].isupper() == True and a not in Titles:
+            Sentences.append(a)
+            x = " ".join(map(str,Sentences))
+            Paragraph.append(x)
+            Sentences = []
+        else:
+            Sentences.append(a)
+    except IndexError:
+        break
+x = " ".join(map(str,Sentences))
+Paragraph.append(x)
+for b in Paragraph:
+    print(b)
